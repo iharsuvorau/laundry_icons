c0e48e064f43ce1c512e051c7cba055d60228edf
Ihar Suvorau <igotbrains@gmail.com>
2015-11-18 01:51:54 +0300

    Core features implemented

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# laundry_icons
+# Laundry Icons
+
+Laundry Icons is a front-end web application built with Backbone.js and KimonoAPI. Data is extracted from Wikipedia and converted into API with Kimono Labs.
---
+++ app.js
@@ -0,0 +1,118 @@
+(function($) {
+
+  // Backbone.sync = function(method, model, success, error) {
+  //   success();
+  // }
+
+  //
+  // Models + Collections
+  //
+
+  var IconModel = Backbone.Model.extend({
+
+    defaults: {
+      icon: null,
+      title: null,
+      index: null,
+    },
+
+  });
+
+  var LaundryCollection = Backbone.Collection.extend({
+
+    model: IconModel,
+    url: 'https://www.kimonolabs.com/api/d1l8cliq?apikey=QC8Xzp85dDnMT5AWCRe63aCF4EicyGuM',
+    parse: function(response) {
+      //console.log('fetched:', response.results.washing);
+      //return response.results.washing;
+      data = response.results;
+      extracted = [];
+      _.each(data, function(big) {
+        for (var i=0; i<big.length; i++) {
+          extracted.push(big[i]);
+        }
+      });
+      return extracted
+    },
+
+  });
+
+  //
+  // Views
+  //
+  
+  var IconView = Backbone.View.extend({
+    
+    tagName: 'div',
+    className: 'item',
+    template: _.template($('#icon-template').html()),
+
+    events: {
+      'mouseover': 'showTip',
+      'click': 'highlight',
+      'mouseout': 'resetTip',
+    },
+
+    initialize: function() {
+    },
+
+    render: function() {
+      this.$el.html(this.template(this.model.toJSON()));
+      return this;
+    },
+
+    showTip: function() {
+      var tip = $('.tip');
+      tip.text(this.model.get('title'));
+    },
+
+    resetTip: function() {
+      var tip = $('.tip');
+      tip.text('Laundry Symbols');
+    },
+
+    highlight: function() {
+      this.$el.toggleClass('highlighted');
+    },
+
+  });
+
+  var LaundryView = Backbone.View.extend({
+    
+    el: '.collection',
+
+    events: {
+    },
+
+    initialize: function() {
+      var self = this;
+      this.listenTo(this.collection, 'sync', this.render);
+    },
+
+    render: function() {
+      var $list = this.$el.add('ul');
+
+      this.collection.each(function(model) {
+        var item = new IconView({model: model});
+        $list.append(item.render().$el);
+      }, this); 
+
+      return this;
+    },
+
+    showTip: function() {
+    },
+
+  });
+
+  //
+  // R U N
+  //
+  
+  var items = new LaundryCollection();
+  var itemsView = new LaundryView({collection: items});
+  items.fetch({
+    dataType: 'jsonp'
+  });
+
+})(jQuery);
